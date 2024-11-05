693d948527e8b1afb01fd164c532592d08d37efa
gyum8651 <gyum8651@gmail.com>
2024-11-05 23:42:13 +0900

    학교 서버용 코드 최적화

--- scheduler/daily_scheduler.py
+++ scheduler/daily_scheduler.py
@@ -47,7 +47,7 @@ def daily_prediction_task():
 
 # 스케줄 시작
 def start_scheduler():
-    schedule.every().day.at("20:25").do(daily_prediction_task)
+    schedule.every().day.at("18:00").do(daily_prediction_task)
     while True:
         schedule.run_pending()
         time.sleep(60)
